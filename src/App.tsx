fb9f894f9dbc964e9f23068232c8f333e1aaf1ee
X22U008 <165369242+X22U008@users.noreply.github.com>
2025-05-19 11:37:27 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  // ここにコンポーネントのロジックを書く
+  // 例: const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>Hello World!!</p>
+    </>
+  )
+}
+
+export default App
